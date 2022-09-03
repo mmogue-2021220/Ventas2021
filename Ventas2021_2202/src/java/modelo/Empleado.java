c209edcc30f00031044a27bdd00b1b5c167f9fd7
mmogue-2021220 <mmogue-2021220@kinal.edu.gt>
2022-09-03 12:35:18 -0600

    se actualizo la rama develop

--- Ventas2021_2202/src/java/modelo/Empleado.java
+++ Ventas2021_2202/src/java/modelo/Empleado.java
@@ -2,5 +2,41 @@
 package modelo;
 
 public class Empleado {
+    private int codigoEmpleado;
+    private String nombresEmpleado;
+    private String telefonEmpleado;
+
+    public Empleado() {
+    }
+
+    public Empleado(int codigoEmpleado, String nombresEmpleado, String telefonEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+        this.nombresEmpleado = nombresEmpleado;
+        this.telefonEmpleado = telefonEmpleado;
+    }
+
+    public int getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(int codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    public String getNombresEmpleado() {
+        return nombresEmpleado;
+    }
+
+    public void setNombresEmpleado(String nombresEmpleado) {
+        this.nombresEmpleado = nombresEmpleado;
+    }
+
+    public String getTelefonEmpleado() {
+        return telefonEmpleado;
+    }
+
+    public void setTelefonEmpleado(String telefonEmpleado) {
+        this.telefonEmpleado = telefonEmpleado;
+    }
     
 }
